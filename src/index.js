570c69625d330a78564961b2bb113f8e4d40c82e
Egor <e_budnik@mail.ru>
2018-09-30 23:29:36 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let zero = 0;
+	while(number>0){
+		number = number / 5;
+		number = Math.floor(number);
+		zero += number;
+	}
+
+    return zero;
 }
